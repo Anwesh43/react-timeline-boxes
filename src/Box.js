66c85387652ab89e761aa0a33be5a2905d74160c
Anwesh43 <anweshthecool0@gmail.com>
2020-04-05 22:26:48 +0530

    adding Box component

---
+++ src/Box.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const boxWidthFactor = 8
+const staticColor = '#9E9E9E'
+const dynamicColor = '#3F51B5'
+
+const boxStyle = (i, hSize, w, scale, color) => {
+    const wSize = w / boxWidthFactor
+    const width = `${wSize}px`
+    const height = `${hSize * scale}px`
+    const top = `${hSize * i}px`
+    const left = `${w / 2 - wSize / 2}px`
+    const position = 'absolute'
+    const background = color
+    const color = 'white'
+    const textAlign = 'center'
+    return {width, height, position, left, top, background, color, textAlign}
+}
+
+
+export const Box = ({i, scale, hSize, w, color, children}) => {
+    return (<div style = {boxStyle(i, hSize, w, scale, color)}>
+              {children}
+          </div>)
+}
+
+export const TimeLineBox =  (props) => {
+    return <React.Fragment>
+
+            <Box key = {`static_${i}`} {...props} color = {staticColor} scale = {1} i = {props.i + 2}/>
+            <Box key = {`dynamic_${i}`} {...props} color = {dynamicColor} i = {props.i + 2}/>
+        </React.Fragment>
+}
+
+export const StartingBox = (props) => {
+    return <Box {...props} scale = {1} color = {dynamicColor}>
+          Next
+      </Box>
+}
