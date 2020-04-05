731412e4dfbac8e546abaa7c4540518f8838d444
Anwesh43 <anweshthecool0@gmail.com>
2020-04-05 23:24:03 +0530

    adding TimelineBoxComponent class

---
+++ src/TimelineBoxComponent.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Line from './Line'
+import {TimeLineBox} from './Box'
+import {divideScale} from './utils'
+
+const TimeLineBox = ({w, h, scale, hSize, i}) => {
+    const scBox = divideScale(scale, 0, 2)
+    const scLine = divideScale(scale, 1, 2)
+    return <div>
+              <TimeLineBox i = {i} w = {w} hSize = {hSize} scale = {scBox}/>
+              <Line w = {w} h = {h} scale = {scale} hSize = {hSize}/>
+          </div>
+}
+
+export default TimeLineBox
